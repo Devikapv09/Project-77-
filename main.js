af6eb73a69642bf39e3cfd2648651224806d584e
Devikapv09 <86156260+Devikapv09@users.noreply.github.com>
2021-10-26 19:09:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("Hi! I am Mr. Console.")
+
+var first_name = "Alan";
+console.log(first_name);
+
+var last_name = "Walker";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 86;
+var b = 92;
+console.log(a + b)
+
+var a = 86;
+var b = 92;
+console.log(a - b)
+
+var a = 86;
+var b = 92;
+console.log(a * b)
+
+var a = 86;
+var b = 92;
+console.log(a / b)
